1ff65a90c49cc06fde2e302348fe1ae16857ebd3
ilona <gogiashvili.ilona@gmail.com>
2024-04-11 22:29:48 -0400

    commit tree 5772214fa1a6027a4ba25357e4735f179aa76ed5

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+  let headquarters = 42;
+  return distance - headquarters;
+}
+function distanceFromHqInFeet(distanceFeet) {
+  return distanceFromHqInBlocks(distanceFeet) * 264;
+}
